2d4c9cf44c81fe683f1a91366ff4cab94e2af39f
Greg <greg_j@live.com>
2021-11-29 23:03:10 +0100

    config changes

--- smarthome_entryform/form.js
+++ smarthome_entryform/form.js
@@ -1,4 +1,4 @@
-class App extends HTMLElement {
+class SmartHomeMeterReading extends HTMLElement {
 
     constructor() {
         super();
@@ -15,22 +15,32 @@ class App extends HTMLElement {
             this.innerHTML = "Provide valid 'data-submit-uri' attribute!";
         } else {
             this.innerHTML = "" +
+                "<div class='smart-home-form'> " +
                 "<form action='#' name='smart_home_form' id='smart_home_form'>" +
-                " <label for='type'>Reading type:</label>" +
-                " <select id='type' name='type' required='required'>" +
-                "   <option value='' >---select one---</option>" +
-                "   <option value='POWER_READING'>Power</option>" +
-                "   <option value='GAS_READING'>Gas</option>" +
-                "   <option value='HOME_WATER_READING'>Home Water</option>" +
-                "   <option value='GARDEN_WATER_READING'>Garden Water</option>" +
-                " </select>" +
-                " <label for='readingDate'>Reading date:</label>" +
-                " <input type='date' id='readingDate' name='readingDate' required>" +
-                " <label for='value'>Value:</label>" +
-                " <input type='number' id='value' name='value' required>" +
-                " <input type='submit' value='SAVE'>" +
-            "</form>" +
-            "<div id='result-msg'></div>";
+                " <div class='smart-home-row'>" +
+                "   <label for='type'>Reading type:</label>" +
+                "   <select id='type' name='type' required='required'>" +
+                "     <option value='' >---select one---</option>" +
+                "     <option value='POWER_READING'>Power</option>" +
+                "     <option value='GAS_READING'>Gas</option>" +
+                "     <option value='HOME_WATER_READING'>Home Water</option>" +
+                "     <option value='GARDEN_WATER_READING'>Garden Water</option>" +
+                "   </select>" +
+                " </div>" +
+                " <div class='smart-home-row'>" +
+                "   <label for='readingDate'>Reading date:</label>" +
+                "   <input type='date' id='readingDate' name='readingDate' required>" +
+                " </div>" +
+                " <div class='smart-home-row'>" +
+                "   <label for='value'>Value:</label>" +
+                "   <input type='number' id='value' name='value' required>" +
+                " </div>" +
+                " <div class='smart-home-row'>" +
+                "   <input type='submit' value='SAVE'>" +
+                " </div>" +
+                "</form>" +
+                "<div id='result-msg' class='smart-home-result'></div>" +
+                "</div>";
 
             this.addEventListener("submit", (e) => this.formSubmit(e, accessToken, url));
         }
@@ -80,10 +90,6 @@ class App extends HTMLElement {
     }
 }
 
-//if (window.customElements.get('my-app-element') === undefined) {
-//  console.log("already loaded!");
-//} else {
-  console.log("loading custom element!");
-  window.customElements.define('my-app-element', App);
-//}
+console.log("loading custom element!");
+window.customElements.define('smart-home-meter-reading', SmartHomeMeterReading);
 console.log("loaded!");
